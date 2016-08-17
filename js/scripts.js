39ba8558a6a9c57bb2cc38763ceae315f24a3cd6
Amber Farrington and Adam Gorbahn <adam@>
2016-08-17 09:56:50 -0700

    make the pig latin translator work and apply uppercase to the new index 0 character

--- js/scripts.js
+++ js/scripts.js
@@ -6,27 +6,34 @@ $(document).ready(function () {
 
     console.log(holder);
 
+    newWord = "";
+
     for (var i = 0; i < holder.length; i++) {
       if (holder[i].indexOf("Qu") != -1) {
-          var num = 2;
+        var num = 2;
       } else {
         var num = 1;
       }
 
       var firstPart = holder[i].substr(0, num);
       var lowerpart = holder[i].substr(num);
+
+      if (firstPart.charAt(0) === firstPart.charAt(0).toUpperCase()) {
+        lowercaseUpper = lowerpart.charAt(0).toUpperCase();
+      } else {
+        lowercaseUpper = lowerpart.charAt(0);
+      }
+
       firstPart = firstPart.toLowerCase();
-      holder[i] = lowerpart.charAt(0).toUpperCase() + lowerpart.slice(1) + firstPart + "ay";
-    }
+      holder[i] = lowercaseUpper + lowerpart.slice(1) + firstPart + "ay";
 
-    newWord = "";
 
-    for (var i = 0; i < holder.length; i++) {
       if (i === holder.length - 1) {
         newWord += holder[i];
       } else {
         newWord += holder[i] + " ";
       }
+
     }
 
     $("#output").text(newWord);
